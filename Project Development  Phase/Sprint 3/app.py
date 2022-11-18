cb79d19b10f797db09e10ce22e54fae43b2b19eb
Balaji N <67403977+bjgold12@users.noreply.github.com>
2022-11-18 22:53:28 +0530

    Add files via upload

---
+++ Project Development  Phase/Sprint 3/app.py
@@ -0,0 +1,164 @@
+from flask import Flask, render_template, request, redirect, url_for, session,flash
+import os
+import ibm_db
+from flask_mail import Mail, Message
+
+
+conn= ibm_db.connect("DATABASE=bludb;HOSTNAME=764264db-9824-4b7c-82df-40d1b13897c2.bs2io90l08kqb1od8lcg.databases.appdomain.cloud;PORT=32536;SECURITY=SSL;SSLServerCertificate=DigiCertGlobalRootCA.crt;UID=rsj36993;PWD=9Yvt1EV6cb3DgObg",'','')
+
+app = Flask(__name__)
+
+
+
+
+@app.route("/",methods=['GET'])
+def home():
+  print(os.environ.get('MAIL_DEFAULT_SENDER'))
+  if 'email' not in session:
+    return redirect(url_for('login'))
+  return render_template('home.html',name='Home')
+
+
+@app.route("/Recepientabout",methods=['GET'])
+def Recepientabout():
+  return render_template('Recepientabout.html')
+
+@app.route("/Recepienthome",methods=['GET'])
+def Recepienthome():
+  return render_template('Recepienthome.html')
+
+
+
+@app.route("/register",methods=['GET','POST'])
+def register():
+  if request.method == 'POST':
+
+    try:
+      email = request.form['email']
+      username = request.form['username']
+      password = request.form['password']
+      userType = request.form['type']   
+
+      if not email or not username or not password:
+        return render_template('register.html',error='Please fill all fields')
+      
+      #hash=bcrypt.hashpw(password.encode('utf-8'),bcrypt.gensalt())
+
+      query = "SELECT * FROM USERS WHERE Email=?"
+      stmt = ibm_db.prepare(conn, query)
+      ibm_db.bind_param(stmt,1,email)
+      ibm_db.execute(stmt)
+      isUser = ibm_db.fetch_assoc(stmt)
+      
+      print("entering1")
+      if not isUser:
+        insert_sql = "INSERT INTO Users(Name,email,PASSWORD,usertype) VALUES (?,?,?,?)"
+        prep_stmt = ibm_db.prepare(conn, insert_sql)
+        ibm_db.bind_param(prep_stmt, 1, username)
+        ibm_db.bind_param(prep_stmt, 2, email)
+        ibm_db.bind_param(prep_stmt, 3, password)
+        ibm_db.bind_param(prep_stmt, 4, userType)
+        ibm_db.execute(prep_stmt)
+        # print("entering2")
+
+
+        return render_template('register.html',success="You can login")
+      else:
+        return render_template('register.html',error='Invalid Credentials')
+
+    except Exception as e:
+      print("error",e)
+
+  return render_template('register.html',name='Home')
+
+@app.route("/login",methods=['GET','POST'])
+def login():
+  if request.method == 'POST':
+    email = request.form['email']
+    password = request.form['password']
+
+    if not email or not password:
+      return render_template('login.html',error='Please fill all fields')
+    query = "SELECT * FROM USERS WHERE Email=?"
+    stmt = ibm_db.prepare(conn, query)
+    ibm_db.bind_param(stmt,1,email)
+    ibm_db.execute(stmt)
+    isUser = ibm_db.fetch_assoc(stmt)
+
+    if(isUser and isUser["USERTYPE"]=="Donor"):
+      return render_template('Donorhome.html',error='Invalid Credentials')
+
+    if(isUser and isUser["USERTYPE"]=="Recepient"):
+      return render_template('Recepienthome.html',error='Invalid Credentials')
+
+    if not isUser:
+      return render_template('login.html',error='Invalid Credentials')
+      
+    #isPasswordMatch = bcrypt.checkpw(password.encode('utf-8'),isUser['PASSWORD'].encode('utf-8'))
+
+    #if not isPasswordMatch:
+    if(isUser['PASSWORD']!=password):
+      return render_template('login.html',error='Invalid Credentials')
+
+    session['email'] = isUser['EMAIL']
+    return redirect(url_for('home'))
+
+  return render_template('login.html',name='Home')
+
+
+@app.route('/logout')
+def logout():
+  session.pop('email', None)
+  return redirect(url_for('login'))
+
+
+
+
+@app.route('/request',methods=['GET','POST'])
+def req():
+  if request.method == 'GET':
+    return render_template('request.html',name='request')
+  email = request.form['email']
+  name = request.form['Name']
+  phone = request.form['phone']
+  BloodGroupReq = request.form['BloodGroup']
+  Address = request.form['Address']
+  #to_email = To(email)
+  print(email,name,phone,BloodGroupReq,Address)
+  query = "SELECT * FROM DONORS WHERE BloodGroup=?"
+  stmt = ibm_db.prepare(conn, query)
+  ibm_db.bind_param(stmt,1,BloodGroupReq)
+  ibm_db.execute(stmt)
+  ll = ibm_db.fetch_assoc(stmt)
+  if(ll):
+    listt = []
+    while(ll!=False):
+      listt.append(ll)
+      ll = ibm_db.fetch_assoc(stmt)
+    print(listt)
+
+    donor_list = []
+    for i in listt:
+        donor_list.append(i["EMAIL"])
+
+    try:  
+      msg = Message('Urgent!! Plasma needed!!', recipients=donor_list)
+      
+      content = "This email is sent from Plasma Donor Application! \n"+"Request for plasma was made by: \n"+"Name: "+name+"\n Email: "+email+"\n Phone: "+phone+"\n Address: "+Address+"\n Please contact for further communications!"
+      
+      msg.body = content
+      msg.html = f'<p>This email is sent from Plasma Donor Application!</p> \n <p>Request for plasma was made by: {name}</p> \n <p>Phone number: {phone}</p>\n <p>Address: {Address}</p> '
+      mail.send(msg)
+      print("mail successfully sent")
+    except Exception as e:
+      print("error: ",e)
+    	
+    return render_template('reqReplyS.html',name='reqReplyS',total=len(listt))
+  else:  
+    return render_template('reqReplyF.html',name='reqReplyF')
+
+
+app.debug = True
+
+if __name__ == "__main__":
+  app.run(host="0.0.0.0")
